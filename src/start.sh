e2d669d58da8a75b8c6fee0f45a173e92d39552b
Xavier Mignot <code@xmi.fr>
2023-11-10 15:05:53 -0500

    feat: Update dockerfile

--- src/start.sh
+++ src/start.sh
@@ -8,7 +8,7 @@ cd /home/docker/actions-runner || exit
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token "${REG_TOKEN}"
+    ./config.sh remove --token "${REG_TOKEN}"
 }
 
 trap 'cleanup; exit 130' INT
