4d127f631788788ffcc00ac22427fa394c09be99
Xavier Mignot <code@xmi.fr>
2023-11-29 22:06:47 -0500

    feat: Add ghcr image

--- src/start.sh
+++ src/start.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/"${ORGANIZATION}"/actions/runners/registration-token | jq .token --raw-output)
+REG_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/"${ORG_NAME}"/actions/runners/registration-token | jq .token --raw-output)
 
-cd /home/docker/actions-runner || exit
-
-./config.sh --url https://github.com/"${ORGANIZATION}" --token "${REG_TOKEN}"
-
-cleanup() {
-    echo "Removing runner..."
-    ./config.sh remove --unattended --token "${REG_TOKEN}"
-}
-
-trap 'cleanup; exit 130' INT
-trap 'cleanup; exit 143' TERM
-
-./run.sh & wait $!
+./config.sh --url https://github.com/"${ORG_NAME}" --token "${REG_TOKEN}" --unattended --ephemeral && ./run.sh
